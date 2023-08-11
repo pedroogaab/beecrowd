e1149b6b73c282bcd40d793a59be4730f3a65f40
pedroogaab <plechi_2016@hotmail.com>
2023-08-11 14:50:32 -0300

    add comentary

--- Dijkstra.py
+++ Dijkstra.py
@@ -4,8 +4,8 @@ while True:
     try:
         l.append(input())
         
-    except EOFError:
+    except EOFError: #Gera este erro quando nao tem mais dados dentro do arquivo que esta sendo acessado
         break
         
-l = set(l)
+l = set(l) #set vira um dicionario e pega o elemento apenas uma vez, se repetir ele nao pega mais. EX: {"A", "a", "2", "1"}
 print(len(l))
