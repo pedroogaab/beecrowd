3a6b137eba6f6e892a6a1e004fd23461e60b42ea
pedroogaab <plechi_2016@hotmail.com>
2023-09-15 17:40:10 -0300

    1012 beecrownd

---
+++ Area_1012.py
@@ -0,0 +1,9 @@
+a, b, c = input().split()
+
+a, b, c = float(a), float(b), float(c)
+
+print(f"TRIANGULO: {(c*a)/2:.3f}") # area do traingulo retangulo = (catAd*catOp)/2 >> multilplicação dos catetos e dividido por 2
+print(f"CIRCULO: {3.14159*(c**2):.3f}") # area = pi*r² >> pi vezes o raio ao quadrado
+print(f"TRAPEZIO: {(a+b)*(c/2):.3f}") # area = (a+b)h/2 >> bases vezes a altura dividido por dois
+print(f"QUADRADO: {b**2:.3f}") # area = a² >> lado*lado 
+print(f"RETANGULO: {a*b:.3f}") #area = a*b >> largura vezes altura 
